5b736374124fa3151f13fcaaca8da56e255d0726
muhammadrahimxon <muhammadrahimxonabdurahmonov@gmail.com>
2024-11-28 17:41:40 +0500

    ha

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./"],
   theme: {
-    extend: {},
+    extend: {
+
+    },
   },
   plugins: [],
 }
